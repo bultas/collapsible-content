21817cdaf8a0bbcaf48827ad494e7f7ce24b7268
bultas <danielbultas@gmail.com>
2020-04-26 15:48:07 +0200

    update demo

--- scripts.js
+++ scripts.js
@@ -9,3 +9,13 @@ window.addEventListener("click", ({ target: { dataset } }) => {
     document.getElementById(dataset.collapsible).toggleAttribute("open");
   }
 });
+
+const addButton = document.getElementById("add-dynamic-content");
+
+addButton.addEventListener("click", () => {
+  const p = document.createElement("p");
+  p.innerText =
+    "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer rutrum, orci vestibulum ullamcorper ultricies, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer rutrum, orci vestibulum ullamcorper ultricies";
+
+  document.getElementById("collapsible-2").appendChild(p);
+});
